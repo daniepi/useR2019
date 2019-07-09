c1a1ccf52e6338258c11710bd780a3d68123752a
daniepi <dano.piacek@gmail.com>
2019-07-09 10:02:23 +0200

    Adding roxygen documentation

--- R/fbind.R
+++ R/fbind.R
@@ -1,3 +1,8 @@
+#' @title Binding two factors
+#' @param x first factor
+#' @param y second factor
+#' @return a character vector
+#'
 fbind <- function(x, y) {
     return(c(as.character(x), as.character(y)))
 }
