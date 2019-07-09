b4d3314fea4e28179bc83a78a50f9c12a2b178c5
daniepi <dano.piacek@gmail.com>
2019-07-09 09:57:32 +0200

    Updating DESCRIPTION file

--- script.R
+++
@@ -1,8 +0,0 @@
-library(data.table)
-ip <- as.data.table(installed.packages())
-ip[Priority == "base", Package]
-ip[Priority != "base", Package]
-ip[is.na(Priority), Package]
-ip[, .N, by = c("LibPath", "Priority")]
-
-##
